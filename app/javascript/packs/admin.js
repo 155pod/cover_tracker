4e32b3270f7e338a1896a093111e10721b179240
John Hawthorn <john@hawthorn.email>
2021-03-07 19:05:18 -0800

    Do archiving using ajax

--- app/javascript/packs/admin.js
+++ app/javascript/packs/admin.js
@@ -4,3 +4,17 @@ import AudioPlayer from "audio_player"
 
 Rails.start()
 AudioPlayer.start()
+
+window.addEventListener("load", () => {
+  document.querySelectorAll(".js-archive-cover").forEach((button) => {
+    const form = button.form;
+    const container = document.getElementById(button.getAttribute("data-target"))
+    form.addEventListener("ajax:before", (event) => {
+      container.classList.add("deleting");
+      container.classList.add("pe-none");
+    });
+    form.addEventListener("ajax:success", (event) => {
+      container.parentNode.removeChild(container);
+    });
+  });
+});
